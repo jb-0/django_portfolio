6a8bb0d335af6d83d67a382942d0e92601bd2672
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-07-03 08:04:09 +0100

    Customised admin class list views

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 from .models import Skill, Profile, WorkExperience, ProjectExperience
 
-
 # Register your models here.
-admin.site.register(Skill)
-admin.site.register(Profile)
-admin.site.register(WorkExperience)
-admin.site.register(ProjectExperience)
+#admin.site.register(Skill)
+#admin.site.register(Profile)
+#admin.site.register(WorkExperience)
+#admin.site.register(ProjectExperience)
+
+
+# Customise admin classes
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('skill','category')
+
+
+@admin.register(WorkExperience)
+class WorkExperienceAdmin(admin.ModelAdmin):
+    list_display = ('start_date', 'company', 'job_title')
+
+
+@admin.register(ProjectExperience)
+class ProjectExperienceAdmin(admin.ModelAdmin):
+    list_display = ('project_title', 'programming_languages')
