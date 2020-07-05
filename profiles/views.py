678863f54ae744d0a3669063d212203bc316c658
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-07-05 15:47:24 +0100

    Session attribute generated for anonymous users, offers page count

--- profiles/views.py
+++ profiles/views.py
@@ -8,9 +8,14 @@ def index(request):
     num_profiles = Profile.objects.count()
     num_skills = Skill.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_profiles': num_profiles,
-        'num_skills': num_skills
+        'num_skills': num_skills,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
