deb4f1673657ad11b3ee97c0cd7899d827d2e7c6
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-07-05 20:48:53 +0100

    Improved layout and styling, introduced cards for profile list

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.about, name='about'),
-    path('profiles/', views.ProfileListView.as_view(), name='profiles'),
-    path('profiles/<int:pk>', views.ProfileDetailView.as_view(), name='profile-detail'),
+    path('<int:pk>', views.ProfileDetailView.as_view(), name='profile-detail'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -5,20 +5,17 @@ from django.views import generic
 
 # Create your views here.
 def about(request):
-    num_profiles = Profile.objects.count()
-    num_skills = Skill.objects.count()
+    # num_profiles = Profile.objects.count()
+    # num_skills = Skill.objects.count()
+    # profiles = Profile.objects.all()
 
     context = {
-        'num_profiles': num_profiles,
-        'num_skills': num_skills,
+        'num_profiles': Profile.objects.count(),
+        'num_skills': Skill.objects.count(),
+        'profiles': Profile.objects.all()
     }
 
     return render(request, 'about.html', context=context)
-
-
-class ProfileListView(generic.ListView):
-    model = Profile
-    context_object_name = 'my_profile_list'  # your own name for the list as a template variable
 
 
 class ProfileDetailView(generic.DetailView):
