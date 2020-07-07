137902d33bcece45fad354810f9da16c5c402fdd
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-07-07 08:49:52 +0100

    Custom form created allowing generation of new profile

---
+++ profiles/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from .models import Skill
+
+
+class NewProfileForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    about = forms.CharField()
+
+    choice_values = Skill.objects.values_list('id', 'skill')
+    skills = forms.MultipleChoiceField(choices=choice_values)
+
+    # We should always clean user input
+    def clean_profile_data(self):
+        data = {'name': self.cleaned_data['name'],
+                'email': self.cleaned_data['email'],
+                'about': self.cleaned_data['about'],
+                'skills': self.cleaned_data['skills']}
+
+        if not data['name']:
+            raise ValidationError(_('Invalid name - name field is blank'))
+
+        # TODO any further validation?
+
+        return data
+
--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.about, name='about'),
     path('<int:pk>', views.ProfileDetailView.as_view(), name='profile-detail'),
+    path('new-profile/', views.new_profile, name='new-profile')
 ]
